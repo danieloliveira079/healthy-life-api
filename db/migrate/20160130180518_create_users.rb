64506e5f29f8ca85c5b5f8533587baea63b21baa
Daniel Oliveira <daniel.oliveira079@gmail.com>
2016-02-27 13:12:07 -0300

    email unique

--- db/migrate/20160130180518_create_users.rb
+++ db/migrate/20160130180518_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :email, index: true
+      t.string :email
       t.string :password_digest
 
       t.timestamps null: false
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160131165351) do
+ActiveRecord::Schema.define(version: 20160227160610) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,7 +48,7 @@ ActiveRecord::Schema.define(version: 20160131165351) do
     t.datetime "updated_at",      null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
   add_foreign_key "authentication_tokens", "users"
   add_foreign_key "campaigns", "users"
