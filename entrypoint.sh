66046e69015f30c99f39c0170801306a7eeb5fdf
Daniel Oliveira <daniel.oliveira079@gmail.com>
2016-05-03 23:23:00 -0300

    entrypoint

---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Install dependencies
+bundle check > /dev/null 2>&1 || bundle install
+
+# Create database
+has_db=$(psql -h $DATABASE_HOST -U postgres -lqt | cut -d \| -f 1 | grep -w healthy-api_development | wc -l )
+
+if [ $has_db != 0 ]; then
+  echo "Existing database migrate only"
+  bundle exec rake db:migrate
+else
+  echo "Non-Existing database setup"
+  bundle exec rake db:setup
+fi
+
+bundle exec rails s -p 3000 -b 0.0.0.0
